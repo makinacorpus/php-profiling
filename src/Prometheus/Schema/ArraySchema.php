005c58436b70cf81a6e8814ee3d9acc4ff34d7e3
Pierre Rineau <pierre.rineau@makina-corpus.com>
2024-05-14 14:57:31 +0200

    add runtime validation in prometheus array schema implementation

--- src/Prometheus/Schema/ArraySchema.php
+++ src/Prometheus/Schema/ArraySchema.php
@@ -4,6 +4,8 @@ declare (strict_types=1);
 
 namespace MakinaCorpus\Profiling\Prometheus\Schema;
 
+use MakinaCorpus\Profiling\Prometheus\Error\SchemaError;
+
 class ArraySchema extends Schema
 {
     public function __construct(string $namespace, array $data, bool $debug = false)
@@ -11,44 +13,89 @@ class ArraySchema extends Schema
         parent::__construct($namespace, $debug);
 
         foreach ($data as $name => $def) {
-            match ($def['type']) {
+            match ($def['type'] ?? null) {
                 'counter' => $this->registerCounter(
                     new CounterMeta(
-                        name: $name,
-                        labels: $def['labels'] ?? [],
+                        active: (bool) ($def['active'] ?? true),
                         help: $def['help'] ?? null,
-                        active: (bool) ($def['active'] ?? true)
+                        labels: $this->validateLabels($name, $def['labels'] ?? []),
+                        name: $name,
                     )
                 ),
                 'gauge' => $this->registerGauge(
                     new GaugeMeta(
-                        name: $name,
-                        labels: $def['labels'] ?? [],
+                        active: (bool) ($def['active'] ?? true),
                         help: $def['help'] ?? null,
-                        active: (bool) ($def['active'] ?? true)
+                        labels: $this->validateLabels($name, $def['labels'] ?? []),
+                        name: $name,
                     )
                 ),
                 'summary' => $this->registerSummary(
                     new SummaryMeta(
-                        name: $name,
-                        labels: $def['labels'] ?? [],
-                        help: $def['help'] ?? null,
                         active: (bool) ($def['active'] ?? true),
-                        quantiles: $def['quantiles'] ?? null,
+                        help: $def['help'] ?? null,
+                        labels: $this->validateLabels($name, $def['labels'] ?? []),
                         maxAge: $def['max_age'] ?? null,
+                        name: $name,
+                        quantiles: $this->validateQuantiles($name, $def['quantiles'] ?? null),
                     )
                 ),
                 'histogram' => $this->registerHistogram(
                     new HistogramMeta(
-                        name: $name,
-                        labels: $def['labels'] ?? [],
-                        help: $def['help'] ?? null,
                         active: (bool) ($def['active'] ?? true),
-                        buckets: $def['buckets'] ?? null,
+                        buckets: $this->validateBuckets($name, $def['buckets'] ?? null),
+                        help: $def['help'] ?? null,
+                        labels: $this->validateLabels($name, $def['labels'] ?? []),
+                        name: $name,
                     )
                 ),
-                default => null,
+                default => throw new SchemaError(\sprintf("'%s': invalid schema, type '%s' is unknown", $name, $def['type'] ?? '<null>')),
             };
         }
+    }
+
+    private function validateLabels(string $name, mixed $labels): array
+    {
+        if (!\is_array($labels)) {
+            throw new SchemaError(\sprintf("'%s' invalid schema, 'labels' must be an array.", $name));
+        }
+        foreach ($labels as $index => $label) {
+            if (!\is_string($label)) {
+                throw new SchemaError(\sprintf("'%s' invalid schema, 'labels' item #%s must be a string value.", $name, $index));
+            }
+        }
+        return $labels;
+    }
+
+    private function validateBuckets(string $name, mixed $buckets): ?array
+    {
+        if (null === $buckets) {
+            return null;
+        }
+        if (!\is_array($buckets)) {
+            throw new SchemaError(\sprintf("'%s' invalid schema, 'buckets' must be an array.", $name));
+        }
+        foreach ($buckets as $index => $bucket) {
+            if (!\is_numeric($bucket)) {
+                throw new SchemaError(\sprintf("'%s' invalid schema, 'buckets' item #%s must be a int or a float value.", $name, $index));
+            }
+        }
+        return $buckets;
+    }
+
+    private function validateQuantiles(string $name, mixed $quantiles): ?array
+    {
+        if (null === $quantiles) {
+            return null;
+        }
+        if (!\is_array($quantiles)) {
+            throw new SchemaError(\sprintf("'%s' invalid schema, 'quantiles' must be an array.", $name));
+        }
+        foreach ($quantiles as $index => $quantile) {
+            if (!\is_numeric($quantile)) {
+                throw new SchemaError(\sprintf("'%s' invalid schema, 'quantiles' item #%s must be a int or a float value.", $name, $index));
+            }
+        }
+        return $quantiles;
     }
 }
